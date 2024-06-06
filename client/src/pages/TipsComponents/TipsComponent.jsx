70ae5356c7b6bbf9ceacd151731c61292ae20dd2
Sara Bou Saleh <sara.bousaleh@nexera.net>
2024-06-06 16:35:21 +0300

    library finished

---
+++ client/src/pages/TipsComponents/TipsComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TipsComponent = ({ title, tips, photoUrl }) => {
+  return (
+    <div className="dual-container">
+      <div className="text-container">
+        <h2 className='h2-detail'>{title}</h2>
+        <ul>
+          {tips.map((tip, index) => (
+            <li key={index}><p>{tip}</p></li>
+          ))}
+        </ul>
+      </div>
+      <div className="photo-container">
+        <img src={photoUrl} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default TipsComponent;
